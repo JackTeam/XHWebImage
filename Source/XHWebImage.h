574c5347dcf8b6157c934324fc550abe61e87587
xhzengAIB <xhzengAIB@gmail.com>
2014-02-08 22:27:51 +0000

    Add Dlog method

--- Source/XHWebImage.h
+++ Source/XHWebImage.h
@@ -9,5 +9,10 @@
 #ifndef XHWebImage_XHWebImage_h
 #define XHWebImage_XHWebImage_h
 
+#ifdef DEBUG
+#   define DLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#else
+#   define DLog(...)
+#endif
 
 #endif

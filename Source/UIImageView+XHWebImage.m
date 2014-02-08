b6c5d4e4c8cc7f37dc3286f3f8115588229a6acf
xhzengAIB <xhzengAIB@gmail.com>
2014-02-08 23:32:56 +0000

    implementation UIImageView (XHWebImage) logic

--- Source/UIImageView+XHWebImage.m
+++ Source/UIImageView+XHWebImage.m
@@ -7,7 +7,88 @@
 //
 
 #import "UIImageView+XHWebImage.h"
+#import "XHWebImageManager.h"
 
 @implementation UIImageView (XHWebImage)
+
+- (id)initWithImageAtUrl:(NSString *)url placeholder:(UIImage *)placeholder {
+    return [self initWithImageAtUrl:url placeholder:placeholder imageRoundRadius:0];
+}
+
+- (id)initWithImageAtUrl:(NSString *)url placeholder:(UIImage *)placeholder imageRoundRadius:(CGFloat)radius {
+    // Need URL
+    if(!url)
+        return self;
+    
+    if((self = [super init]))
+    {
+        // Setting Placeholder
+        if(placeholder)
+        {
+            [self setImage:placeholder];
+            [self setNeedsLayout];
+        }
+        
+        // Launching downloading
+        [self downloadImageAtUrl:[NSURL URLWithString:url] placeholder:placeholder completeHandler:[self completeBlock] progressHandler:nil];
+    }
+    
+    return self;
+}
+
+- (void)setImageAtUrl:(NSString *)url placeholder:(UIImage *)placeholder {
+    [self setImageAtUrl:url placeholder:placeholder imageRoundRadius:0];
+}
+
+- (void)setImageAtUrl:(NSString *)url placeholder:(UIImage *)placeholder imageRoundRadius:(CGFloat)radius {
+    [self setImageAtUrl:url placeholder:placeholder imageRoundRadius:radius completeHandler:[self completeBlock] progressHandler:nil];
+}
+
+- (void)setImageAtUrl:(NSString *)url placeholder:(UIImage *)placeholder imageRoundRadius:(CGFloat)radius completeHandler:(XHWebImageCompleteHandler)completeBlock progressHandler:(XHWebImageProgressHandler)progressBlock {
+    [self downloadImageAtUrl:[NSURL URLWithString:url] placeholder:placeholder completeHandler:completeBlock progressHandler:progressBlock];
+}
+
+#pragma mark - Internal download function
+
+- (void)downloadImageAtUrl:(NSURL *)url placeholder:(UIImage *)placeholder completeHandler:(XHWebImageCompleteHandler)completeBlock progressHandler:(XHWebImageProgressHandler)progressBlock
+{
+    if(!url)
+        return;
+    
+    // Setting Placeholder
+    if(placeholder)
+    {
+        [self setImage:placeholder];
+        [self setNeedsLayout];
+    }
+    
+    // Launching downloading
+    [[XHWebImageManager shareWebImageManager] downloadImageAtURL:url completeHandler:completeBlock progressHandler:progressBlock];
+}
+
+#pragma mark - Blocks
+// Complete block
+- (XHWebImageCompleteHandler)completeBlock
+{
+    // Keeping reference to self, without retaining it.
+    __weak UIImageView *wself = self;
+    return ^void(UIImage *image, NSError *error){
+        if(error) {
+            NSLog(@"Error : %@", error);
+        }
+        if(wself && image)
+        {
+            dispatch_async(dispatch_get_main_queue(),
+                           ^{
+                               if(wself && image)
+                               {
+                                   [wself setImage:image];
+                                   [wself setNeedsLayout];
+                                   [wself setNeedsUpdateConstraints];
+                               }
+                           });
+        }
+    };
+}
 
 @end
